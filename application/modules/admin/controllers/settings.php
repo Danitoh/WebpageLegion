f61323dc0f15604345905d078469304048582c49
borgotech <botnet@earthling.net>
2017-02-21 08:40:25 +0200

    fix disable expansion ..1

--- application/modules/admin/controllers/settings.php
+++ application/modules/admin/controllers/settings.php
@@ -89,16 +89,24 @@ class Settings extends MX_Controller
 
 		switch($this->input->post('disabled_expansions'))
 		{
+			case "wod":
+				$disabled_expansions = array($this->realms->getEmulator()->getExpansionId("Legion"));
+			break;
+			
+			case "cataclysm":
+				$disabled_expansions = array($this->realms->getEmulator()->getExpansionId("Wod"), $this->realms->getEmulator()->getExpansionId("Legion"));
+			break;
+			
 			case "wotlk":
-				$disabled_expansions = array($this->realms->getEmulator()->getExpansionId("Cataclysm"));
+				$disabled_expansions = array($this->realms->getEmulator()->getExpansionId("Cataclysm"), $this->realms->getEmulator()->getExpansionId("Wod"), $this->realms->getEmulator()->getExpansionId("Legion"));
 			break;
 
 			case "tbc":
-				$disabled_expansions = array($this->realms->getEmulator()->getExpansionId("WotLK"), $this->realms->getEmulator()->getExpansionId("Cataclysm"));
+				$disabled_expansions = array($this->realms->getEmulator()->getExpansionId("WotLK"), $this->realms->getEmulator()->getExpansionId("Cataclysm"), $this->realms->getEmulator()->getExpansionId("Wod"), $this->realms->getEmulator()->getExpansionId("Legion"));
 			break;
 
 			case "none":
-				$disabled_expansions = array($this->realms->getEmulator()->getExpansionId("TBC"), $this->realms->getEmulator()->getExpansionId("WotLK"), $this->realms->getEmulator()->getExpansionId("Cataclysm"));
+				$disabled_expansions = array($this->realms->getEmulator()->getExpansionId("TBC"), $this->realms->getEmulator()->getExpansionId("WotLK"), $this->realms->getEmulator()->getExpansionId("Cataclysm"), $this->realms->getEmulator()->getExpansionId("Wod"), $this->realms->getEmulator()->getExpansionId("Legion"));
 			break;
 
 			default:
